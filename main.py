e08b53367560709f9b43056d222cf419bacdd429
HieptotheHo <hohiep.hpy@gmail.com>
2024-03-20 17:28:42 +0700

    styling nav bar

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import streamlit as st 
 from streamlit_option_menu import option_menu
 
+st.title('Title')
 selected = option_menu (
     menu_title = None,
-    options = ["Login","Chat bot","Payment"],
+    options = ["Login","Chatbot","Payment"],
+    icons = ['bi-info-square','robot','credit-card'],
+    menu_icon="cast",
     default_index =0,
-    orientation = "horizontal"
+    orientation = "horizontal",
 )
