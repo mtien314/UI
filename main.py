ee601ae65de3614519659fc062bd4e336719bf21
mtien314 <90877954+mtien314@users.noreply.github.com>
2024-03-23 23:15:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-import streamlit as st 
+import streamlit as st
+import os
+import streamlit_google_oauth as oauth
+from credentials import client_id, client_secret, redirect_uri
 from streamlit_option_menu import option_menu
+import webbrowser
 
-st.title('Title')
-selected = option_menu (
-    menu_title = None,
-    options = ["Login","Chatbot","Payment"],
-    icons = ['bi-info-square','robot','credit-card'],
-    menu_icon="cast",
-    default_index =0,
-    orientation = "horizontal",
-)
+
+def ggAuth():
+    login_info = oauth.login(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        logout_button_text="Logout",
+    )
+    if login_info:
+        user_id, user_email = login_info
+        selected = option_menu (
+        menu_title = None,
+        options = ['Chat bot 🤖','Payment 💸'],
+        default_index =0,
+        orientation = "horizontal"
+    )
+        st.write(f"Welcome {user_email}")
+        if selected == "Payment":
+            url = "https://buy.stripe.com/test_28o8Ay2a73Wg1ygeUU"
+            webbrowser.open_new_tab(url)
+        
+login_info = ggAuth()
