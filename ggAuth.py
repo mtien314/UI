2b2e01a27503f04311ce91127da126d3e983e2cd
evelynpham04 <phamphuonglinh250104@gmail.com>
2024-03-23 03:06:26 -0400

    Update ggAuth.py

--- ggAuth.py
+++ ggAuth.py
@@ -1,26 +1,21 @@
 import streamlit as st
-from google.oauth2 import id_token
-from google.auth.transport import requests
-
-st.title("Title")
+import os
+import streamlit_google_oauth as oauth
+from credentials import client_id, client_secret, redirect_uri
 
 def ggAuth():
-    
-    # Google Authentication
-    st.subheader("Google Authentication")
-    client_id = "268734706774-ae6maqv35ag8dqbi3h77hhd3q14k77hn.apps.googleusercontent.com"
-    token = st.text_input("Enter your Google ID token", type="password")
-    if st.button("Authenticate"):
-        if not token:  # Check if the token is empty
-            st.warning("Please enter a valid Google ID token")
-        elif not token.isdigit():  # Check if the token is a number
-            st.error("Invalid Google ID token format. Please enter a valid token.")
-        else:
-            try:
-                idinfo = id_token.verify_oauth2_token(token, requests.Request(), client_id)
-                if idinfo['aud'] != client_id:
-                    raise ValueError("Invalid client ID")
-                st.success(f"Authentication successful: {idinfo['name']}")
-            except ValueError as e:
-                st.error("Authentication failed")
-                st.error(e)
+    login_info = oauth.login(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        logout_button_text="Logout",
+    )
+    if login_info:
+        user_id, user_email = login_info
+        st.write(f"Welcome {user_email}")
+
+
+login_info = ggAuth()
+if login_info:
+    user_id, user_email = login_info
+    st.write(f"Welcome {user_email}")
