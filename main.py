74ea17caf4bbf1d83b85f6337358b2b60baf6efc
mtien314 <90877954+mtien314@users.noreply.github.com>
2024-04-13 16:53:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,71 @@
+from os import access
+from more_itertools import last
+from numpy import place
 import streamlit as st
 import webbrowser
 import streamlit_google_oauth as oauth
-from credentials import client_id, client_secret, redirect_uri
 from streamlit_navigation_bar import st_navbar
-import update
-from user_record import update_user_record
+from time import sleep
+from user_record import update_user_record,update_account,check_account,find_accountID
 
 
 url = 'https://buy.stripe.com/test_aEUaH5bhH7PP8nueUU'
 
-
 swich_page = st.session_state.get("page_index",0)
 
 def ggAuth():
-    login_info = oauth.login(
+
+    info =  oauth.login(
         client_id=client_id,
         client_secret=client_secret,
-        redirect_uri=redirect_uri
-    )
-    
-    if login_info:
-        user_id,user_email = login_info
-        update_user_record(user_id,user_email)
-        st.text(f"Welcome {user_email}")
-
+        redirect_uri=redirect_uri)
+    if info:
+        sleep(0.5) 
+        user_id,user_email = info
+        last_visited = 0
+        update_user_record(user_id,user_email,last_visited)
+        update_account(user_id,user_email)
+        st.success("login success")
+        st.switch_page("pages/page1.py")
+    st.session_state.clear()
 
 if swich_page ==0:
     page = st_navbar(['Home','Subscription','Chat','Search','Solutions','Contact','Login'])
-
+    switch = 0
     if page == 'Subscription':
         webbrowser.open_new_tab(url)
-        update.update_customers()
-        update.update_transaction()
-
-
+        #update.update_customers()
+        #update.update_transaction()
 
     if page =="Login":
         ggAuth()
-        st.session_state["page_index"] = 1
 
-if swich_page ==1:
-    page = st_navbar(['Home','Subscription','Chat','Search','Solutions','Contact'])
-    if page =='Home':
-        st.write(f"Welcome")
+        placeholder = st.empty()
+        with placeholder.form("login"):
+            st.markdown("### Login")
+
+            email = st.text_input("Email")
+            pasw = st.text_input("Password", type = "password")
+            
+            actual_pass = check_account(email)
+            submit = st.form_submit_button("login")
+
+        if pasw == actual_pass:
+            st.success("Login success")
+
+            user_id = find_accountID(email)
+            update_user_record(user_id=user_id,user_email=email,last_visited=0)
+            
+            placeholder = st.empty()
+            sleep(0.5)
+            st.switch_page("pages/page1.py")
         
+        elif pasw ==None or actual_pass ==None:
+            st.warning("Please login")
+        elif pasw!=actual_pass:
+            st.warning("Login failed")
+
+
+        
+
+
