a4330efecccbb4a7904632bc62adc57e32462c7e
mtien314 <90877954+mtien314@users.noreply.github.com>
2024-05-02 17:33:33 +0700

    Update page1.py

--- page1.py
+++ page1.py
@@ -5,7 +5,13 @@ from datetime import datetime
 import mysql.connector
 import pandas as pd
 from time import sleep
+import user_record
 from user_record import update_account2,connect
+import bcrypt
+import webbrowser
+import update
+
+url = 'https://buy.stripe.com/test_aEUaH5bhH7PP8nueUU'
 
 page2 = st_navbar(['Home','Subscription','Chat','Search','Solutions','Contact','Logout'])
 
@@ -30,20 +36,55 @@ def update_lastime(last_visited):
         values = (last_visited,id)
         cursor.execute(sql,values)
         conn.commit()
+    
     cursor.close()
 
 
+def update_logout(last):
+   
+    conn = mysql.connector.connect(host = "localhost",
+                               port = "3306",
+                               user = "root",
+                               passwd = "123",
+                               db = "doctor"
+                                )
+    cursor = conn.cursor()
+    users = cursor.execute("SELECT * FROM history_logs")
+    result = cursor.fetchall()
+    df = pd.DataFrame(result,columns =['Email', 'Login', 'Logout', 'Login_status'])
+    n = len(df['Email'])
+    email = df['Email'][n-1]
+    logout = df['Logout'][n-1]
+    log_status = df['Login_status'][n-1]
+
+    if logout =='_':
+        sql = "UPDATE History_logs SET logout = %s WHERE Email = email ORDER BY Login DESC LIMIT 1;"
+        values = (last,)
+        cursor.execute(sql,values)
+        conn.commit()
+
+    status = "Logged Out"
+    if log_status =='Logged In':
+        sql = "UPDATE History_logs SET Login_status = %s WHERE Email = email ORDER BY Login DESC LIMIT 1;"
+        values = (status,)
+        cursor.execute(sql,values)
+        conn.commit()
+
+    cursor.close()
 
 if page2 =="Logout":
 
     current_datetime = datetime.now()
     # Format the datetime object (replace with your preferred format)
     last_visited = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+    update_logout(last_visited)
     update_lastime(last_visited)
     st.session_state.clear()
     st.switch_page("testUI.py")
 
 if page2 == 'Home':
+
     #update password
     result = connect(table ='User')
     df = pd.DataFrame(result,columns =['ID','Email','Register','Last_visited'])
@@ -56,21 +97,34 @@ if page2 == 'Home':
     n2 = len(df2['ID'])
     passw = df2['Password'][n2-1]
 
-    if pd.isnull(regist)==False and passw =='0':
+    if pd.isnull(regist)== False and passw =='0':
         placeholder = st.empty()
      
         with placeholder.form("Change password"):
             st.markdown("### Change password")
             new_pass = st.text_input("New password", type = "password")
-            pasw = st.text_input("Enter password again", type = "password")
+            password = st.text_input("Enter password again", type = "password")
             submit = st.form_submit_button("submit")
-        if submit and new_pass != pasw:
+
+            hash_passw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+        if submit and new_pass != password:
             st.warning("Please check new_pass and pass again")
-        if submit and new_pass ==pasw:
+        if submit and new_pass == password:
             st.success("Change password success")
             sleep(0.5)
-            update_account2(new_pass)
+            update_account2(hash_passw)
             placeholder.empty()
 
     if passw !='0':
         st.write('Welcome')
+
+
+    
+if page2 =='Subscription':
+    webbrowser.open_new_tab(url)
+
+    #update customers and transaction
+
+    update.update_customers()
+    update.update_transaction()
